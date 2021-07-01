919852f515d07473843d7f607dd3636b93fd26d8
theaterplus <70135872+theaterplus@users.noreply.github.com>
2021-07-01 14:46:11 +0530

    Update worker-beta-second-domain.js

--- worker/worker-beta-second-domain.js
+++ worker/worker-beta-second-domain.js
@@ -15,8 +15,8 @@ const randomserviceaccount = serviceaccounts[Math.floor(Math.random()*serviceacc
 
 const authConfig = {
     "siteName": "Pirates Drive Index", // Website name
-    "client_id": "643126170682-k2aggk87nb50lnprj6qc4nr9ih5qrglo.apps.googleusercontent.com", // Client id from Google Cloud Console
-    "client_secret": "mfdWLwMVHOUs18xHUamuDkT-", // Client Secret from Google Cloud Console
+    "client_id": "746239575955-oao9hkv614p8glrqpvuh5i8mqfoq145b.apps.googleusercontent.com", // Client id from Google Cloud Console
+    "client_secret": "u5a1CSY5pNjdD2tGTU93TTnI", // Client Secret from Google Cloud Console
     "refresh_token": "", // Authorize token
     "service_account": false, // true if you're using Service Account instead of user account
     "service_account_json": randomserviceaccount, // don't touch this one
